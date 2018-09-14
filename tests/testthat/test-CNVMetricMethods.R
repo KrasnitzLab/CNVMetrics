ed7682d412841821d9a2b79b4cb47cad86cefea2
Astrid Deschenes <adeschen@hotmail.com>
2018-09-14 18:15:01 -0400

    Adding test for calculateWeightedEuclideanDistance() function

--- tests/testthat/test-CNVMetricMethods.R
+++ tests/testthat/test-CNVMetricMethods.R
@@ -2,6 +2,7 @@
 
 library(CNVMetrics)
 library(rtracklayer)
+library(GenomicRanges)
 
 chrInfo <- Seqinfo(seqnames=c("chr1", "chr2", "chr3"),
                    seqlengths=c(10000, 20000, 1500), 
@@ -31,3 +32,32 @@ test_that("prepareInformation() must return error when segmentWithHeader is not 
     expect_error(prepareInformation(segDirectory = "test", chrInfo = chrInfo, 
                                     segmentWithHeader = 444), error_message02)
 })
+
+
+
+### Tests calculateWeigthedEucledianDistance() results
+
+context("calculateWeigthedEucledianDistance() results")
+
+test_that("calculateWeigthedEucledianDistance() must return good results", {
+    
+    dataGR <- GRanges(seqnames = "chr1", 
+                        ranges = IRanges(start=c(1, 30, 50, 56, 101, 180, 200, 221),
+                                         end=c(29, 49, 55, 100, 110, 199, 220, 230)))
+    
+    values(dataGR) <- DataFrame(included = c(rep(TRUE, 2), rep(FALSE, 2), rep(TRUE, 4)), 
+                                  File1 = c(0.1, 0.1, 0.1, NA, NA, NA, 0.5, NA), 
+                                  File2 = c(NA, 0.3, 0.3, 0.3, 0.3, 0.4, 0.4, 0.4))
+    
+    expMatrix <- matrix(c(0, 0.387652570, 0.387652570, 0), byrow = T, 
+                            ncol = 2, nrow = 2)
+    colnames(expMatrix) <- c("File1", "File2")
+    rownames(expMatrix) <- c("File1", "File2")
+    
+    expect_equivalent(calculateWeigthedEucledianDistance(dataGR), expMatrix, 
+                        tolerance=0.000001)
+    
+})
+
+
+
