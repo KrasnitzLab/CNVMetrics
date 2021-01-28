a8eecd8bde4d0901af9fe2c0dbb20f6e33d8222f
Astrid Deschenes <adeschen@hotmail.com>
2021-01-28 10:59:46 -0500

    Adding unit tests for calculateSorensen() method

--- tests/testthat/test-CNVMetricInternalMethods.R
+++ tests/testthat/test-CNVMetricInternalMethods.R
@@ -299,3 +299,132 @@ test_that("calculateSzymkiewicz() must return expected result of 0", {
     
     expect_equal(results, expected)
 })
+
+test_that("calculateSzymkiewicz() must return expected result of NA when two GRanges empty", {
+
+    sample01 <- GRanges()
+    sample02 <- GRanges()
+    
+    result <- CNVMetrics:::calculateSzymkiewicz(sample01, sample02)
+    
+    expected <- NA
+    
+    expect_equal(result, expected)
+})
+
+
+### Tests calculateSorensen() results
+
+context("calculateSorensen() results")
+
+test_that("calculateSorensen() must return expected result of 1 for identical GRanges", {
+    
+    
+    sample01 <- GRanges(seqnames = c("chr1", "chr2", "chr4"), 
+                        ranges =  IRanges(start = c(1905048, 4554832, 31686841), 
+                                          end = c(2004603, 4577608, 31695808)), strand = rep("+", 3))
+    sample02 <- GRanges(seqnames = c("chr1", "chr2", "chr4"), 
+                        ranges =  IRanges(start = c(1905048, 4554832, 31686841), 
+                                          end = c(2004603, 4577608, 31695808)), strand = rep("-", 3))
+    
+    results <- CNVMetrics:::calculateSorensen(sample01, sample02)
+    
+    expected <- 1.0
+    
+    expect_equal(results, expected)
+})
+
+test_that("calculateSorensen() must return expected result for one GRanges included in the other", {
+    
+    
+    sample01 <- GRanges(seqnames = c("chr1", "chr2", "chr4"), 
+                        ranges =  IRanges(start = c(905048, 1554832, 31686841), 
+                                          end = c(2204603, 4577608,41695808)), 
+                        strand = rep("+", 3))
+    sample02 <- GRanges(seqnames = c("chr1", "chr2", "chr4"), 
+                        ranges =  IRanges(start = c(1905048, 4554832, 31686841), 
+                                          end = c(2004603, 4577608, 31695808)), 
+                        strand = rep("-", 3))
+    
+    result <- CNVMetrics:::calculateSorensen(sample01, sample02)
+    
+    expected <- 0.018157313600969
+    
+    expect_equal(result, expected)
+})
+
+test_that("calculateSorensen() must return expected result when first GRanges empty", {
+    
+    sample01 <- GRanges()
+    sample02 <- GRanges(seqnames = c("chr1", "chr2", "chr4"), 
+                        ranges =  IRanges(start = c(1905048, 4554832, 31686841), 
+                                          end = c(2004603, 4577608, 31695808)), 
+                        strand = rep("-", 3))
+    
+    result <- CNVMetrics:::calculateSorensen(sample01, sample02)
+    
+    expected <- 0.000
+    
+    expect_equal(result, expected)
+})
+
+
+test_that("calculateSorensen() must return expected result of NA when two GRanges empty", {
+    
+    ## Create two empty GRanges
+    sample01 <- GRanges()
+    sample02 <- GRanges()
+    
+    result <- CNVMetrics:::calculateSorensen(sample01, sample02)
+    
+    expected <- NA
+    
+    expect_equal(result, expected)
+})
+
+test_that("calculateSorensen() must return expected result of 0.5", {
+    
+    ## Create two GRanges
+    sample01 <- GRanges(seqnames = c("chr1", "chr2", "chr4"), 
+                        ranges =  IRanges(start = c(100, 10, 1000), 
+                                          end = c(199, 19, 1999)), 
+                        strand = rep("-", 3))
+    sample02 <- GRanges(seqnames = c("chr1", "chr2", "chr4"), 
+                        ranges =  IRanges(start = c(150, 15, 1500), 
+                                          end = c(249, 24, 2499)), 
+                        strand = rep("+", 3))
+    
+    result <- CNVMetrics:::calculateSorensen(sample01, sample02)
+    
+    expected <- 0.5
+    
+    expect_equal(result, expected)
+})
+
+
+test_that("calculateSorensen() must return expected result of 0", {
+    
+    ### Create a Seqinfo Object
+    chrInfo <- Seqinfo(seqnames=c("chr1", "chr2", "chr3", "chr4", "chr5"),
+                       seqlengths=rep(2500, 5), 
+                       isCircular=rep(FALSE, 5),
+                       genome="Alien")
+    
+    ## Create two GRanges with no overlap
+    sample01 <- GRanges(seqnames = c("chr1", "chr2", "chr4"), 
+                        ranges =  IRanges(start = c(100, 10, 1000), 
+                                          end = c(199, 19, 1999)), 
+                        strand = rep("-", 3), seqinfo = chrInfo)
+    sample02 <- GRanges(seqnames = c("chr2", "chr3", "chr5"), 
+                        ranges =  IRanges(start = c(150, 15, 1500), 
+                                          end = c(249, 24, 2499)), 
+                        strand = rep("+", 3), seqinfo = chrInfo)
+    
+    result <- CNVMetrics:::calculateSorensen(sample01, sample02)
+    
+    expected <- 0
+    
+    expect_equal(result, expected)
+})
+
+
