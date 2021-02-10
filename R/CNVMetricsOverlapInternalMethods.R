6e007ccb790464d82ed1fc3131ecef82cf9c6e9c
Astrid Deschenes <adeschen@hotmail.com>
2021-02-09 22:27:54 -0500

    Update plotOverlapMetric() and plotOneOverlapMetric() methods
    Former-commit-id: dd8d8d74201792d06e03de71b18d4189ac45f6ed

--- R/CNVMetricsOverlapInternalMethods.R
+++ R/CNVMetricsOverlapInternalMethods.R
@@ -86,6 +86,7 @@
 #'     type="DELETION")
 #' 
 #' @author Astrid Deschênes
+#' @encoding UTF-8
 #' @keywords internal
 calculateOverlapMetric <- function(sample01, sample02, method, type) {
     
@@ -155,6 +156,7 @@ calculateOverlapMetric <- function(sample01, sample02, method, type) {
 #'     
 #' @author Astrid Deschênes
 #' @importFrom GenomicRanges intersect width
+#' @encoding UTF-8
 #' @keywords internal
 calculateSorensen <- function(sample01, sample02) {
     
@@ -220,6 +222,7 @@ calculateSorensen <- function(sample01, sample02) {
 #'     
 #' @author Astrid Deschênes
 #' @importFrom GenomicRanges intersect width
+#' @encoding UTF-8
 #' @keywords internal
 calculateSzymkiewicz <- function(sample01, sample02) {
     
@@ -236,3 +239,106 @@ calculateSzymkiewicz <- function(sample01, sample02) {
     return(result)
 }
 
+#' @title Plot one graph related to metrics based on overlapping 
+#' amplified/deleted regions
+#' 
+#' @description Plot one heatmap of the metrics based on overlapping 
+#' amplified/deleted regions. 
+#' 
+#' @param metric a \code{CNVMetric} object containing the metrics calculated
+#' by \code{calculateOverlapRegionsMetric}.
+#' 
+#' @param type a \code{character} string indicating which graph to generate. 
+#' This should be (an unambiguous abbreviation of) one of  
+#' "\code{AMPLIFICATION}" or "\code{DELETION}".
+#' 
+#' @param show_colnames a \code{boolean} specifying if column names are 
+#' be shown.
+#' 
+#' @param \ldots further arguments passed to 
+#' \code{\link[pheatmap:pheatmap]{pheatmap::pheatmap()}} method.
+#' 
+#' @return a \code{gtable} object containing the heatmap for the specified 
+#' metric.
+#' 
+#' @seealso 
+#' 
+#' The default method  \code{\link[pheatmap:pheatmap]{pheatmap::pheatmap()}}.
+#' 
+#' @examples
+#' 
+#' #' ## Load required package to generate the samples
+#' require(GenomicRanges)
+#' 
+#' ## Create a GRangesList object with 3 samples
+#' ## The stand of the regions doesn't affect the calculation of the metric
+#' demo <- GRangesList()
+#' demo[["sample01"]] <- GRanges(seqnames = "chr1", 
+#'     ranges =  IRanges(start = c(1905048, 4554832, 31686841), 
+#'     end = c(2004603, 4577608, 31695808)), strand =  "*",
+#'     state = c("AMPLIFICATION", "AMPLIFICATION", "DELETION"))
+#' 
+#' demo[["sample02"]] <- GRanges(seqnames = "chr1", 
+#'     ranges =  IRanges(start = c(1995066, 31611222, 31690000), 
+#'     end = c(2204505, 31689898, 31895666)), strand =  c("-", "+", "+"),
+#'     state = c("AMPLIFICATION", "AMPLIFICATION", "DELETION"))
+#' 
+#' ## The amplified region in sample03 is a subset of the amplified regions 
+#' ## in sample01
+#' demo[["sample03"]] <- GRanges(seqnames = "chr1", 
+#'     ranges =  IRanges(start = c(1906069, 4558838), 
+#'     end = c(1909505, 4570601)), strand =  "*",
+#'     state = c("AMPLIFICATION", "DELETION"))
+#' 
+#' ## Calculating Sorensen metric
+#' metric <- calculateOverlapRegionsMetric(demo, method="sorensen")
+#' 
+#' ## Plot both amplification metrics using darkorange color
+#' CNVMetrics:::plotOneOverlapMetric(metric, type="AMPLIFICATION", 
+#'     colorRange=c("white", "darkorange"), show_colnames=FALSE)
+#'
+#' @author Astrid Deschênes
+#' @importFrom pheatmap pheatmap
+#' @importFrom grDevices colorRampPalette
+#' @importFrom methods hasArg
+#' @import GenomicRanges
+#' @encoding UTF-8
+#' @keywords internal
+plotOneOverlapMetric <- function(metric, type, colorRange, show_colnames, ...) 
+{
+    ## Prepare matrix by filling upper triangle
+    metricMat <- metric[[type]]
+    diag(metricMat) <- 1.0
+    metricMat[upper.tri(metricMat)] <- t(metricMat)[upper.tri(metricMat)]
+    
+
+    ## Prepare main title (might not be used if main argument given by user)
+    metricInfo <- switch(attributes(metric)$metric, 
+                         "szymkiewicz"="Szymkiewicz-Simpson", 
+                         "sorensen"="Sorensen")
+    metricInfo <- paste0(type, " - ", metricInfo, " metric")
+    
+    ## Create heatmap
+    ## If color information given, that information is used to create graph
+    ## If main title given, that information is used to create graph
+    if (!hasArg("breaks") && !hasArg("color")) {
+        ## Create color palette using colorRange parameter
+        colors <- colorRampPalette(colorRange)(255)
+        breaks <-  seq(0, 1, length.out=255)
+        
+        if (!hasArg("main")) {
+            pheatmap(metricMat, main=metricInfo, show_colnames=show_colnames, 
+                     color=colors, breaks=breaks, ...)[[4]] 
+        } else {
+            pheatmap(metricMat, show_colnames=show_colnames, 
+                     color=colors, breaks=breaks, ...)[[4]]  
+        }
+    } else {
+        if (!hasArg("main")) {
+            pheatmap(metricMat, main=metricInfo, show_colnames=show_colnames, 
+                        ...)[[4]] 
+        } else {
+            pheatmap(metricMat, show_colnames=show_colnames, ...)[[4]]  
+        }
+    }
+}
