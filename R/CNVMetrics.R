26deb9277dc2255a41ded20d3b10e9fcf440ee28
Astrid Deschenes <adeschen@hotmail.com>
2021-02-10 20:27:03 -0500

    Update package description
    Former-commit-id: a1edfa017381e69000cb0e2373cafef6e53dac0c

--- R/CNVMetrics.R
+++ R/CNVMetrics.R
@@ -1,7 +1,10 @@
 #' CNVMetrics: Copy number variant metrics
 #'
-#' This package implements metrics to facilitate copy number variant 
-#' comparison among samples and/or methods.
+#' The CNVMetrics package calculates similarity metrics to facilitate copy 
+#' number variant comparison among samples and/or methods. Some metrics are 
+#' based on the shared amplified/deleted regions while other metrics rely on 
+#' the level of amplification/deletion. Finally, a visualization tool is 
+#' provided to explore resulting metrics.
 #'
 #' @docType package
 #'
@@ -15,6 +18,14 @@
 #' Maintainer:
 #' Astrid Deschênes <adeschen@hotmail.com>
 #'
+#' @seealso
+#' \itemize{
+#'     \item \code{\link{calculateOverlapMetric}} {for calculating metric 
+#'     using overlapping amplified/deleted regions}
+#'     \item \code{\link{plotOneOverlapMetric}} {for plotting metrics based 
+#'     on overlapping amplified/deleted regions}
+#' }
+#' 
 #' @encoding UTF-8
 #' @keywords package
 NULL
